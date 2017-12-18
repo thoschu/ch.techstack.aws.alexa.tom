163925261ab6cc0988c521f7bee1bc63f1843500
Tom <thoschulte@gmail.com>
2017-12-18 02:20:48 +0100

    add HalloTomIntent

--- index.js
+++ index.js
@@ -138,49 +138,95 @@
 
 const Alexa = require('alexa-sdk');
 
-const APP_ID = 'amzn1.ask.skill.3e7f8ee8-934a-4ee1-93d5-d089ada27053';
+const APP_ID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+
+const languageStrings = {
+    'de': {
+        translation: {
+            FACTS: [],
+            SKILL_NAME: 'Tom´s Hello World Skill',
+            REQUEST_MESSAGE: 'Es ist jetzt %s. Was ist zu tun?',
+            HELP_MESSAGE: 'Momentan ist keine Hilfe implementiert.',
+            HELP_REPROMPT: 'Momentan ist keine Hilfe implementiert.',
+            STOP_MESSAGE: 'In Hamburg sagt man Tschüss!',
+            CANCEL_MESSAGE: 'Abgebrochen. Auf Wiedersehen!'
+        },
+    },
+    'en': {
+        translation: {
+            FACTS: [
+                'A year on Mercury is just 88 days long.',
+                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.'
+            ],
+            SKILL_NAME: 'Tom´s Hello World Skill',
+            REQUEST_MESSAGE: 'It is %s. What can i do?',
+            HELP_MESSAGE: 'No help.',
+            HELP_REPROMPT: 'No help.',
+            STOP_MESSAGE: 'Good bye',
+            CANCEL_MESSAGE: 'Cancel. Bye bye.'
+        },
+    },
+};
+
+let currentTimeFromDate = function(date) {
+    const hours = date.getHours();
+    let value;
+
+    if (hours >= 6 && hours < 12) {
+        value = 'morgens';
+    } else if (hours >= 12 && hours < 14) {
+        value = 'mittags';
+    } else if (hours >= 14 && hours < 18) {
+        value = 'nachmittags';
+    } else if (hours >= 18 && hours < 23) {
+        value = 'abends';
+    } else {
+        value = 'nachts';
+    }
+
+    return value;
+};
 
 let handlers = {
     'LaunchRequest': function() {
-        this.emit('HelloWorldIntent');
-    },
-    'GetNewFactIntent': function() {
-        this.emit('HelloWorldIntent');
+        //this.emit('HelloWorldIntent');
+        const date = new Date();
+        const time = currentTimeFromDate(date);
+        this.emit(':ask', this.t('REQUEST_MESSAGE', time), 'Hallo, was soll ich für dich tun?');
     },
     'SessionEndedRequest': function() {
-        //ToDo - do nothing
+        //ToDo - e.g. do nothing
     },
     'AMAZON.HelpIntent': function() {
-        this.emit(':tell', 'Momentan ist keine Hilfe implementiert.');
+        this.emit(':tell', this.t('HELP_MESSAGE'));
     },
     'AMAZON.CancelIntent': function() {
-        this.emit(':tell', 'Abgebrochen. Auf Wiedersehen!');
+        this.emit(':tell', this.t('CANCEL_MESSAGE'));
     },
     'AMAZON.StopIntent': function() {
-        this.emit(':tell', 'Gestoppt. In Hamburg sagt man Tschüss!');
+        this.emit(':tell', this.t('STOP_MESSAGE'));
     },
     'HelloWorldIntent': function() {
         let txtStr1 = 'Moin, ich bin Thomas. ',
-            txtStr2 = 'Softwareentwickler aus Hamburg.';
+            txtStr2 = 'Softwareentwickler aus Hamburg.',
+            hh = this.event.request.intent.slots.time.value.substr(0, 2),
+            mm = this.event.request.intent.slots.time.value.substr(3, 2),
+            date  = new Date(1977, 7, 13, hh, mm, 0, 0);
 
-        if (this.event.request.intent &&
-            this.event.request.intent.slots &&
-            this.event.request.intent.slots.time &&
-            this.event.request.intent.slots.time.value) {
+        if ( this.event.request.intent.slots.time.value) {
             let tempText = ' Der Value-Slot lautet: <say-as interpret-as="time">',
                 tempValue = this.event.request.intent.slots.time.value + '</say-as>';
-            this.emit(':tell', txtStr1 + txtStr2 + tempText + tempValue);
-        }
-        else {
+            this.emit(':tell', txtStr1 + txtStr2 + tempText + tempValue + '. Es ist also demnach: ' + currentTimeFromDate(date));
+        } else {
             this.emit(':ask', txtStr1 + txtStr2 + ' Kommando?', 'Hallo, das Kommando bitte!');
         }
-
-    },
+    }
 };
 
 exports.handler = function(event, context) {
     const alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
+    alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
